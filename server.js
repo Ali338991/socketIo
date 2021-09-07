0f5e320e425a5a869cbe7b8a5e0677dde2207825
Ali338991 <ali.ansari7268@gmail.com>
2021-09-07 09:28:04 +0400

    Add Script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser")
 var dbConnection = require("./config/Db");
+const port = process.env.PORT || 5000;
 // Routes List
 // var ordersRoute = require("./components/orders/OrdersRoutes");
 // var AuthRoutes = require("./components/auth/AuthRoutes");
@@ -37,10 +38,10 @@ app.use("/superAdmin/teachers", teacherRoutes);
 // ip:port/orders - get
 
 // server port listener
-app.listen("5000", (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log("something went wrong", error);
   } else {
-    console.log("server is working on port, 5000");
+    console.log(`server is working on port, ${port}`);
   }
 });
