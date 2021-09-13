c3dd5df59aae95e3e6a08784bac37d801592a608
Ali338991 <ali.ansari7268@gmail.com>
2021-09-13 16:35:53 +0400

    code defendent

--- components/superAdmin/admins/AdminController.js
+++ components/superAdmin/admins/AdminController.js
@@ -1,128 +1,190 @@
 var AdminList = require("./AdminModel");
-let {sendEmail}=require('../../../utils/sendEmail');
+let { sendEmail } = require('../../../utils/sendEmail');
 
-//Function to add Admin
+//Add Admin
 module.exports.addAdmin = async (req, res) => {
-  const { name, userName, email, mobile, role, status } = req.body;
-  const filename = req.file?.filename;
-  if (!name || !userName || !email || !mobile || !role || !status) {
-    res.status(400).send("All params are required");
-  }
-  const newAdminList = new AdminList({
-    name, userName, email, mobile, role,
-    status:"fullControl",
-    image: filename,
-  });
-  newAdminList.save((err, success) => {
-    if (err) {
-      res.status(501).send("error happen");
+  if (!req.body?.name) {
+    res.status(400).json({ status: "error", message: "Name required", statusCode: 400 })
+    return
+  } else if (!req.body?.userName) {
+    res.status(400).json({ status: "error", message: "User Name Required", statusCode: 400 })
+    return
+  } else if (!req.body?.email) {
+    res.status(400).json({ status: "error", message: "Email Required", statusCode: 400 })
+    return
+  } else if (!req.body?.mobile) {
+    res.status(400).json({ status: "error", message: "Mobile Number  Required", statusCode: 400 })
+    return
+  } else {
+    const check = await AdminList.findOne({ email: req.body?.email });
+    if (check) {
+      res.status(400).json({ status: "success", message: "Email already exist", statusCode: 400 })
+      return
     }
-    res.status(200).send("Admin Added Successfully");
-    sendEmail(email);
-  });
+    const filename = req.file?.filename;
+    const { name, userName, email, mobile } = req.body;
+    const newAdminList = new AdminList({
+      name, userName, email, mobile,
+      status: "fullControl",
+      role: "Admin",
+      image: filename,
+    });
+    const id = newAdminList?._id
+    const yourRole = newAdminList?.role
+    const portal = process.env.AdminPortal
+    await sendEmail(id, portal, email, name, yourRole, res);
+    newAdminList.save((err, success) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err?.message, statusCode: 400 })
+        return
+      }
+      res.status(201).json({ status: "success", message: "Admin Created and Send Email Successfully", statusCode: 201 })
+      return
+    });
+  }
 };
-
-
+//Get List of Admin
 module.exports.getAdminList = async (req, res) => {
-  const getAdminList = await AdminList.find({});
-  res.status(200).json(getAdminList);
+  try {
+    const getAdminList = await AdminList.find({});
+    res.status(202).json({ status: "success", message: "Get list of Admin Successfully", data: { getAdminList }, statusCode: 202 })
+    return
+  } catch (error) {
+    res.status(400).json({ status: "success", message: { error }, statusCode: 400 })
+    return
+  }
 };
 //temporaryBlok
 module.exports.temporaryBlok = async (req, res) => {
-  const { id } = req.body;
-  if (!id) {
-    res.status(400).send("All params are required");
-  }
-  const Admin = await AdminList.findByIdAndUpdate(id, {
-    Status: "temporaryBlok",
-  }, { new: true }
-  )
-  if (!Admin) {
-    res.status(400).send("Your id is incorrect");
-  }
-  Admin.save((err, data) => {
-
-    if (err) {
-      res.status(501).send("error happen");
+  if (!req.body?.id) {
+    res.status(400).json({ status: "error", message: "id required", statusCode: 400 })
+    return
+  } else {
+    const { id } = req.body;
+    const Admin = await AdminList.findByIdAndUpdate(id, {
+      status: "temporaryBlok",
+    }, { new: true }
+    )
+    if (!Admin) {
+      res.status(400).json({ status: "error", message: "Your id is incorrect", statusCode: 400 })
+      return
     }
-    res.status(200).send("Admin Temporary Blok Successfully");
-  });
-};
+    Admin.save((err, data) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err?.message, statusCode: 400 })
+        return
+      }
+      res.status(201).json({ status: "success", message: "Admin temporary blok Successfully", statusCode: 201 })
+      return
+    });
 
+  }
+};
+//Permanent Blok 
 module.exports.permanentBlok = async (req, res) => {
-  const { id } = req.body;
-
-  if (!id) {
-    res.status(400).send("All params are required");
-  }
-  const Admin = await AdminList.findByIdAndUpdate(id, {
-    Status: "permanentBlok",
-  }, { new: true }
-  )
-  if (!Admin) {
-    res.status(400).send("Your id is incorrect");
-  }
-  Admin.save((err, data) => {
-
-    if (err) {
-      res.status(501).send("error happen");
+  if (!req.body?.id) {
+    res.status(400).json({ status: "error", message: "id required", statusCode: 400 })
+    return
+  } else {
+    const { id } = req.body;
+    const Admin = await AdminList.findByIdAndUpdate(id, {
+      status: "permanentBlok",
+    }, { new: true }
+    )
+    if (!Admin) {
+      res.status(400).json({ status: "error", message: "Your id is incorrect", statusCode: 400 })
+      return
     }
-    res.status(200).send("Admin Permanent Blok Successfully");
-  });
+    Admin.save((err, data) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err?.message, statusCode: 400 })
+        return
+      }
+      res.status(201).json({ status: "success", message: "Admin permanent blok Successfully", statusCode: 201 })
+      return
+    });
+  }
 };
-
+//fullControl
 module.exports.fullControl = async (req, res) => {
-  const { id } = req.body;
-
-  if (!id) {
-    res.status(400).send("All params are required");
-  }
-  const Admin = await AdminList.findByIdAndUpdate(id, {
-    Status: "fullControl",
-  }, { new: true }
-  )
-  if (!Admin) {
-    res.status(400).send("Your id is incorrect");
-  }
-  Admin.save((err, data) => {
-    if (err) {
-      res.status(501).send("error happen");
+  if (!req.body?.id) {
+    res.status(400).json({ status: "error", message: "id required", statusCode: 400 })
+    return
+  } else {
+    const { id } = req.body;
+    const Admin = await AdminList.findByIdAndUpdate(id, {
+      status: "fullControl",
+    }, { new: true }
+    )
+    if (!Admin) {
+      res.status(400).json({ status: "error", message: "Your id is incorrect", statusCode: 400 })
+      return
     }
-    res.status(200).send("Now Admin has full Control");
-  });
-};
-
-module.exports.updateAdmin = async (req, res) => {
-  const { name, userName, email, mobile, role, status,id } = req.body;
-  const filename = req.file?.filename;
-  if (!id || !name || !userName || !email || !mobile || !role || !status) {
-    res.status(400).send("All params are required");
+    Admin.save((err, data) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err?.message, statusCode: 400 })
+        return
+      }
+      res.status(201).json({ status: "success", message: "Now Admin has FullControl", statusCode: 201 })
+      return
+    });
   }
+};
+//updateAdmin
+module.exports.updateAdmin = async (req, res) => {
+  if (!req.body?.id) {
+    res.status(400).json({ status: "error", message: "id required", statusCode: 400 })
+    return    
+  }else if (!req.body?.name) {
+    res.status(400).json({ status: "error", message: "Name required", statusCode: 400 })
+    return
+  } else if (!req.body?.userName) {
+    res.status(400).json({ status: "error", message: "User Name Required", statusCode: 400 })
+    return
+  } else if (!req.body?.email) {
+    res.status(400).json({ status: "error", message: "Email Required", statusCode: 400 })
+    return
+  } else if (!req.body?.mobile) {
+    res.status(400).json({ status: "error", message: "Mobile Number  Required", statusCode: 400 })
+    return
+  }else{
+  const { name, userName, email, mobile,id } = req.body;
+  const filename = req.file?.filename;
   const Admin = await AdminList.findByIdAndUpdate(id, {
-    name, userName, email, mobile, role, status,
+    name, userName, email, mobile,
     image: filename,
   }, { new: true }
   )
-  if (!Admin) {
-    res.status(400).send("Your id is incorrect");
-  }
-  Admin.save((err, data) => {
-    if (err) {
-      res.status(501).send("error happen");
+  console.log("admin",Admin);
+   if (!Admin) {
+      res.status(400).json({ status: "error", message: "Your id is incorrect", statusCode: 400 })
+      return
     }
-    res.status(200).send("update Successful");
-  });
-};
-
-module.exports.deleteAdmin = async (req, res) => {
-  const {id} = req.body;   
- 
-  if (!id) {
-    res.status(400).send("All params are required");
-  }
-  const Admin = await AdminList.findByIdAndDelete({_id:id});
-  if (!Admin) {
-    res.status(400).send("Your id is incorrect");
+    Admin.save((err, data) => {
+      if (err) {
+        res.status(400).json({ status: "error", message: err?.message, statusCode: 400 })
+        return
+      }
+      res.status(201).json({ status: "success", message: "Update Admin Successfully", statusCode: 201 })
+      return
+    });
   }  
-  res.status(200).send("Admin Delete Successfully");
+};
+//Delete Admin
+module.exports.deleteAdmin = async (req, res) => {
+  if (!req.body?.id) {
+    res.status(400).json({ status: "error", message: "id required", statusCode: 400 })
+    return    
+  }else{
+    const { id } = req.body;
+    const Admin = await AdminList.findByIdAndDelete({ _id: id });
+    if (!Admin) {
+      res.status(400).json({ status: "error", message: "Your id is incorrect", statusCode: 400 })
+return
+    }
+    res.status(201).json({ status: "success", message: "Admin  Delete Successfully", statusCode: 201 })
+    return
+  }
+
+
 };
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,23 +4,49 @@ const MailerSend = require("mailersend");
 const mailersend = new MailerSend({
   api_key: process.env.apiKey,
 });
-module.exports.sendEmail = async (email) => {
+module.exports.sendEmail = async (id,portal,email,name,yourRole,res) => {
   try{
-    const recipients = [
-      new Recipient("your@domain.com", "Your Client")
+    const recipients = [new Recipient(email,name )];
+
+    const variables = [
+      {
+        email: email,
+        substitutions: [
+          {
+            var: 'id',
+            value: id
+          },
+          {
+            var: 'Role',
+            value: yourRole
+          },
+          {
+            var: 'name',
+            value: name
+          },
+          {
+            var: 'portalLink',
+            value: portal
+          },
+          {
+            var: 'support_email',
+            value: 'info@trainings.techloset.com'
+          }
+        ],
+      }
     ];
-  
+    
     const emailParams = new EmailParams()
-      .setFrom("your@domain.com")
-      .setFromName("domain")
-      .setRecipients(recipients)
-      .setSubject("Subject")
-      .setHtml("This is the HTML content")
-      .setText("This is the text content");
-    await mailersend.send(emailParams);
-    console.log(email);
+        .setFrom("info@trainings.techloset.com")
+        .setFromName("trainings.techloset")
+        .setRecipients(recipients)
+        .setSubject("Job information")
+        .setTemplateId('pr9084zx7xlw63dn')
+        .setVariables(variables);    
+        // await mailersend.send(emailParams);
   }
   catch(error){
-    console.log(error.message);
+    res.status(400).json({status:"error", message:{error}, statusCode: 400})
+    return
   }
 };
